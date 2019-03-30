05627260ee6eb1cca18828104b1e2b619198c570
Patrick Ziegler <zipat@t-online.de>
2019-03-30 13:12:13 +0100

    Minor renaming

--- test/finite/test_PolyRing.py
+++ test/finite/test_PolyRing.py
@@ -15,14 +15,14 @@ class PolyRingTest(unittest.TestCase):
         self.assertEqual(PolyRing(13), PolyMod2(1, 1, 0))
 
     def test_poly_ring_2(self):
-        GF2 = PolyRing.factory(modulo=2, power=3)
+        GF8 = PolyRing.factory(modulo=2, power=3)
         PolyMod2 = Poly.factory(modulo=2)
 
         p1 = PolyMod2(1, 0, 0, 1, 1)
         p2 = PolyMod2(1, 1, 0, 1, 0)
 
-        p1gf2 = GF2(p1)
-        p2gf2 = GF2(p2)
+        p1gf2 = GF8(p1)
+        p2gf2 = GF8(p2)
 
         s = p1 + p2
         sgf2 = p1gf2 + p2gf2
@@ -31,14 +31,14 @@ class PolyRingTest(unittest.TestCase):
         self.assertEqual(sgf2, PolyMod2(1, 0))
 
     def test_poly_ring_3(self):
-        GF2 = PolyRing.factory(modulo=2, power=3)
+        GF8 = PolyRing.factory(modulo=2, power=3)
         PolyMod2 = Poly.factory(modulo=2)
 
         p1 = PolyMod2(1, 0, 1)
         p2 = PolyMod2(1, 1, 1)
 
-        p1gf2 = GF2(p1)
-        p2gf2 = GF2(p2)
+        p1gf2 = GF8(p1)
+        p2gf2 = GF8(p2)
 
         s = p1 * p2
         sgf2 = p1gf2 * p2gf2
